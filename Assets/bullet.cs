4dfebdbd39fd5b0947f13bec8715f497633e4a1c
gdbaradit <gdbaradit@gmail.com>
2020-05-21 21:20:14 -0300

    Initial commit

---
+++ Assets/bullet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    public GameObject hitEffect;
+    public GameObject objeto;
+
+    
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+        Destroy(effect, 5f); 
+        Destroy(gameObject);
+        objeto = col.gameObject;
+        Debug.Log("Está chocando con " + objeto);
+    }
+
+    
+
+
+}
---
+++ Assets/controlDeMovimiento.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System; 
+
+public class controlDeMovimiento : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float bustersSpeed = 2f;
+    public float rotationSpeed = 5f;
+    public Vector2 direccion;
+
+    public float rotacion;
+    public float rotEnRad; 
+    public float pi; 
+
+    
+
+    public Rigidbody2D rb;
+
+    public float movement;
+    float rotador; 
+
+        
+
+    // Update is called once per frame
+    // UPDATE PARA INPUTS
+    void Update()
+    {
+        rotador = Input.GetAxisRaw("Horizontal");
+        movement = Input.GetAxisRaw("Vertical"); 
+    }
+
+    // FIXED UPDATE PARA LAS FÍSICAS
+    void FixedUpdate()
+    {
+        rotacion = rb.rotation;
+        pi = Mathf.PI;
+        rotEnRad = rotacion * pi / 180; 
+
+        direccion = new Vector2 (-1*Mathf.Sin(rotEnRad), Mathf.Cos(rotEnRad));
+
+        if (Input.GetAxisRaw("Horizontal") != 0)
+        {
+
+            
+            rb.rotation = rb.rotation + rotador * rotationSpeed;
+        }
+
+        else if (Input.GetAxisRaw("Horizontal") == 0)
+        {
+          
+        }
+
+        
+       
+
+        
+       
+        
+
+        rb.AddForce(direccion * movement * bustersSpeed);
+    }
+}
